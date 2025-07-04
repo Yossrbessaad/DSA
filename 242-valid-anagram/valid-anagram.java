4cf90b87bf4e50609105dab5351bb93e9ef73ee6
Yossrbessaad <148229159+Yossrbessaad@users.noreply.github.com>
2025-07-04 17:27:02 +0100

    Time: 4 ms (77.55%) | Memory: 44.9 MB (25.29%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -8,7 +8,16 @@ class Solution {
         Arrays.sort(sSort);
         Arrays.sort(tSort);
         return Arrays.equals(sSort, tSort);
-        
+        /*
+        2nd solution using HashMap
+        HashMap<String,Integer> sCount = new HashMap<>();
+        HashpMap<String,Integer> tCount = new HashMap<>();
+        for(int i=0; i<s.length();i++=){
+            sCount.put(s.charAt(i), sCount.getOrDefault(s.charAt(i),0)+1);
+            tCount.put(s.chatAt(i), tCount.getOrDefault(t.charAt(i),0)+1);
+        }
+        return sCount.equals(tCount);
+        */
 
     }
 }
